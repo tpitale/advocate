fa6a553636241c5fa9da1f025a5adb3bdac52575
Tony Pitale <tpitale@gmail.com>
2016-10-22 14:48:46 -0500

    Create an event

--- app/models/event.rb
+++ app/models/event.rb
@@ -7,4 +7,15 @@ class Event < ActiveRecord::Base
   def self.interactions_for(user)
     where(user: user).select(:interaction_id).uniq
   end
+
+  def save
+    super.tap do |result|
+      queue_for_outbound if result
+    end
+  end
+
+  private
+  def queue_for_outbound
+    # send to twilio
+  end
 end
