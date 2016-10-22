fe35139a1a5135a3788dd183d51b19097b931fa6
Tony Pitale <tpitale@gmail.com>
2016-10-22 16:11:47 -0500

    Simple referral creation

--- app/controllers/users/referrals_controller.rb
+++ app/controllers/users/referrals_controller.rb
@@ -1,11 +1,22 @@
 module Users
   class ReferralsController < Users::BaseController
 
+    respond_to :html
+
     def new
+      @referral = Referral.new(
+        code: Referral.generate_code, interaction_id: params[:interaction_id], service_id: params[:service_id])
     end
 
     def create
+      @referral = Referral.create(params[:referral].permit(:code, :interaction_id, :service_id).merge(status: "open"))
+
+      respond_with @referral, location: [:users, current_interaction]
     end
 
+    private
+    def current_interaction
+      @current_interaction ||= Interaction.find(params[:interaction_id] || params[:referral][:interaction_id])
+    end
   end
 end
