3a6f2426bfd3d3b4ff7f5c8c11a5f17007269aae
Andrew Glass <andrewglass1@gmail.com>
2016-10-22 17:59:32 -0500

    remove comment

--- app/controllers/users/services_controller.rb
+++ app/controllers/users/services_controller.rb
@@ -7,15 +7,6 @@ module Users
       @services.each{|s|s.qualify_and_disqualify(@interaction.client)}
       @services = @services.sort_by{|s|s.sort_number}
     end
-
   end
 
-private
-
 end
-
-
-# Emergency Shelter", "Emergency Shelter", "Emergency Shelter", "Emergency Shelter",
-#  "Emergency Shelter", "Emergency Shelter", "Emergency Shelter", "Emergency Shelter",
-#  "Food Programs & Pantries", "Food Programs & Pantries", "Food Programs & Pantries",
-#  "Food Programs & Pantries", "Food Programs & Pantries", "Utility Assistance", "Utility Assistance", "Utility Assistance
