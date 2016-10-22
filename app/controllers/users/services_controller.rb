6000756b9381271b0262b8325f4a9aed74fd0c7f
Andrew Glass <andrewglass1@gmail.com>
2016-10-22 16:59:11 -0500

    render services

--- app/controllers/users/services_controller.rb
+++ app/controllers/users/services_controller.rb
@@ -2,8 +2,15 @@ module Users
   class ServicesController < Users::BaseController
 
     def index
-      @services = ServiceSearch.new(params).results
+      @services = Service.where(name: params["service_name"])
+      # raise Service.all.map(&:name).uniq.sort.inspect
+      # # @services = ServiceSearch.new(params).results
     end
 
   end
 end
+
+# Emergency Shelter", "Emergency Shelter", "Emergency Shelter", "Emergency Shelter",
+#  "Emergency Shelter", "Emergency Shelter", "Emergency Shelter", "Emergency Shelter",
+#  "Food Programs & Pantries", "Food Programs & Pantries", "Food Programs & Pantries",
+#  "Food Programs & Pantries", "Food Programs & Pantries", "Utility Assistance", "Utility Assistance", "Utility Assistance
