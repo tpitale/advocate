031111e2acaf1264957de09889ad90d97918d61d
Tony Pitale <tpitale@gmail.com>
2016-10-22 13:19:12 -0500

    spike out some interaction controller stuff

--- app/controllers/users/interactions_controller.rb
+++ app/controllers/users/interactions_controller.rb
@@ -1,13 +1,25 @@
 module Users
   class InteractionsController < Users::BaseController
 
+    respond_to :html
+
     def index
+      @interactions = Interaction.active_for(current_user)
     end
 
     def show
+      @interaction = Interaction.find(params[:id])
+      @client = @interaction.client
+    end
+
+    def new
+      @interaction = Interaction.new
     end
 
     def create
+      @interaction = Interaction.create_for_phone(params[:interaction][:phone])
+
+      respond_with @interaction, location: [:users, @interaction]
     end
 
   end
