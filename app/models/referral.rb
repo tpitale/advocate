24dcb119f64aed1527822bbbd0ebe05299f7c087
Tony Pitale <tpitale@gmail.com>
2016-10-22 16:11:47 -0500

    Simple referral creation

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -1,2 +1,11 @@
 class Referral < ActiveRecord::Base
+  belongs_to :interaction
+  belongs_to :service
+
+  DIGITS = 1_000_000
+
+  # TODO: do more to prevent code collision
+  def self.generate_code
+    (SecureRandom.random_number * DIGITS).to_i
+  end
 end
