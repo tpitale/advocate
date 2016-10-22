5713e0f6c8861be941eb0e0bb6e003acaf93fac3
Andrew Glass <andrewglass1@gmail.com>
2016-10-22 17:58:24 -0500

    render services and sort based on eligibility

--- app/controllers/users/services_controller.rb
+++ app/controllers/users/services_controller.rb
@@ -2,13 +2,18 @@ module Users
   class ServicesController < Users::BaseController
 
     def index
-      @services = Service.where(name: params["service_name"])
-      # raise Service.all.map(&:name).uniq.sort.inspect
-      # # @services = ServiceSearch.new(params).results
+      @services = Service.where(name: params[:service_name].strip)
+      @interaction = Interaction.find(params[:interaction_id])
+      @services.each{|s|s.qualify_and_disqualify(@interaction.client)}
+      @services = @services.sort_by{|s|s.sort_number}
     end
 
   end
+
+private
+
 end
+
 
 # Emergency Shelter", "Emergency Shelter", "Emergency Shelter", "Emergency Shelter",
 #  "Emergency Shelter", "Emergency Shelter", "Emergency Shelter", "Emergency Shelter",
