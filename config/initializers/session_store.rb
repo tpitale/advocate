b76b1e27ccb024e0bd7d3e22f3370b6fe8c86882
Tony Pitale <tpitale@gmail.com>
2016-11-09 18:50:24 -0600

    Rename app to Advocate

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-Rails.application.config.session_store :cookie_store, key: '_counselor_session'
+Rails.application.config.session_store :cookie_store, key: '_advocate_session'
